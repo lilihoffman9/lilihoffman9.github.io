e511ad08aef0eaf780f9d1c9eaccbb36aa5c6ff2
lilihoffman9 <104588696+lilihoffman9@users.noreply.github.com>
2022-05-03 14:06:56 -0400

    Update deadPig.js

--- deadPig.js
+++ deadPig.js
@@ -16,7 +16,7 @@ function drawGame(){
 
 document.body.onkeyup = function(e){
     if(e.keyCode == 32){
-        window.open("/Users/lilihoffman/Desktop/Game/indexPig.html", "_self")
+        window.open("indexPig.html", "_self")
     }
 }
 
