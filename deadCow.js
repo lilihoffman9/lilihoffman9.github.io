641d5660ffd81c418ff6b136453be1e2c20ac0de
lilihoffman9 <104588696+lilihoffman9@users.noreply.github.com>
2022-05-03 14:05:08 -0400

    Update deadCow.js

--- deadCow.js
+++ deadCow.js
@@ -16,7 +16,7 @@ function drawGame(){
 
 document.body.onkeyup = function(e){
     if(e.keyCode == 32){
-        window.open("/Users/lilihoffman/Desktop/Game/indexCow.html", "_self")
+        window.open("indexCow.html", "_self")
     }
 }
 
