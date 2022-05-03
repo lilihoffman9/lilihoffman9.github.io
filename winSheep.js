80ad57977385a41c0d8bab86f4e3bf8501491532
lilihoffman9 <104588696+lilihoffman9@users.noreply.github.com>
2022-05-03 14:30:50 -0400

    Update winSheep.js

--- winSheep.js
+++ winSheep.js
@@ -16,7 +16,7 @@ function drawGame(){
 
 document.body.onkeyup = function(e){
     if(e.keyCode == 32){
-        window.open("/Users/lilihoffman/Desktop/Game/sheepLevel2.html", "_self")
+        window.open("sheepLevel2.html", "_self")
     }
 }
 
