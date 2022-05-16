61d496eaa364f26337fe31d5e09c01e211505071
lilihoffman9 <liliflower9@icloud.com>
2022-05-16 13:41:06 -0400

    195 to win

--- indexChicken.js
+++ indexChicken.js
@@ -70,7 +70,7 @@ function drawGame(){
 
     animal.drawChicken();
     
-    if(animal.getYPosition() <= 180){
+    if(animal.getYPosition() <= 195){
         window.open("winChicken.html", "_self")
     }
 
